851e0e61b2eda57cb2df4db7c8eec33857b079e7
Kathy Dieser <katdiese@gmail.com>
2016-04-15 09:55:00 -0600

    added promises, updated routes

--- src/server/routes/students.js
+++ src/server/routes/students.js
@@ -5,62 +5,75 @@ var Students = require('../models/students');
 
 //GET ALL students
 router.get('/', function(req, res, next) {
-  Students.find({}, function(err, students) {
-    if(err) {
-      return next(err);
-    }
-    res.status(200)
-    .json({
+  Students.find({})
+  .then(function(students) {
+    res.status(200).json({
       status: 'success',
       data: students
-    });
+    })
+  })
+  .catch(function(err) {
+    return next(err);
   })
 });
 
+//GET one student
 router.get('/:id', function(req, res, next) {
-  Students.find({_id: req.params.id}, function(err, student) {
-    if(err) {
+  Students.find({_id: req.params.id})
+    .then(function(student) {
+      res.status(200).json({
+        status: 'success',
+        data: student
+      })
+    })
+    .catch(function(err) {
       return next(err);
-    }
+    })
+});
+
+//DELETE one student
+router.delete('/:id', function(req, res, next) {
+  Students.remove({_id: req.params.id})
+  .then(function(student) {
     res.status(200)
     .json({
       status: 'success',
       data: student
-    });
-  });
-});
-
-router.delete('/:id', function(req, res, next) {
-  Students.remove({_id: req.params.id}, function(err) {
-    if(err) {
-      return next(err);
-    }
-    res.status(200).json({
-      status: 'success'
     })
-  });
+  })
+  .catch(function(err) {
+      return next(err);
+    })
 });
 
+//UPDATE one student
 router.put('/update/:id', function(req, res, next) {
-  Students.findByIdAndUpdate( req.params.id, req.body, { new: true }, function(err, update) {
-    if (err) { return next(err) };
+  Students.findByIdAndUpdate( req.params.id, req.body, { new: true })
+  .then(function(student) {
     res.status(200)
     .json({
-      status : 'success',
-      data : update
-    });
-  });
-
+      status: 'success',
+      data: student
+    })
+  })
+  .catch(function(err) {
+    return next(err);
+  })
 });
 
+//ADD one student
 router.post('/', function(req, res, next){
  var student = Students(req.body);
- student.save(function(error, response){
-   res.status(200).json({
-     status: 'success',
-     data: response
-   });
- });
+ student.save()
+ .then(function(student) {
+    res.status(200).json({
+      status: 'success',
+      data: student
+    })
+ })
+ .catch(function(err) {
+  return next(err);
+ })
 });
 
 
